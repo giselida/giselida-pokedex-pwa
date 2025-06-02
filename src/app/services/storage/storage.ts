00efd00e95ea2fc539b84b66114b603435a5901c
Giselida Melo <giselidac@gmail.com>
2025-06-02 09:53:32 -0300

    ✨ feat: create local storage service

---
+++ src/app/services/storage/storage.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  storage = localStorage;
+
+  setItem<T>(key: string, value: T) {
+    this.storage.setItem(key, JSON.stringify(value));
+  }
+  getItem(key: string) {
+    let value;
+    const keyStoraged = this.storage.getItem(key);
+    if (!keyStoraged) return undefined;
+    try {
+      value = JSON.parse(keyStoraged);
+    } catch {
+      value = keyStoraged;
+    }
+    return value;
+  }
+}
